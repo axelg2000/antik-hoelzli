747fe10498882c199297185795ca9607b0335639
axel <axelg2000@gmail.com>
2025-06-06 12:02:08 +0200

    addd boto s3

--- antik_hoelzli/shared_settings/dev_psql.py
+++ antik_hoelzli/shared_settings/dev_psql.py
@@ -13,3 +13,18 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+INSTALLED_APPS += ['storages']
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-central-1')  # replace with your region
+
+# Optional: make uploaded media publicly accessible
+AWS_QUERYSTRING_AUTH = False
+
+# Optional: set default ACL to public-read
+AWS_DEFAULT_ACL = 'public-read'
--- antik_hoelzli/shared_settings/prod_psql.py
+++ antik_hoelzli/shared_settings/prod_psql.py
@@ -3,6 +3,21 @@ from .base import *
 import os
 import dj_database_url
 
+INSTALLED_APPS += ['storages']
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'eu-central-1')  # replace with your region
+
+# Optional: make uploaded media publicly accessible
+AWS_QUERYSTRING_AUTH = False
+
+# Optional: set default ACL to public-read
+AWS_DEFAULT_ACL = 'public-read'
+
 DEBUG = False
 ALLOWED_HOSTS = [os.getenv('PRODUCTION_HOST', 'yourdomain.com')]
 
